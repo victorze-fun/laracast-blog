065f59c9cc0bddac706f97b21b40e3c343f48dde
Victor Zevallos <victorze@protonmail.com>
2022-01-27 15:32:47 -0500

    Refactor

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("post.{$slug}", 1200, fn() => file_get_contents($path));
-
-    return view('post', ['post' => $post]);
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 })->where('post', '[A-z_\-]+');
